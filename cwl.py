a706179a8dc09c2e4039f2ebfb53fd2b53e73be8
pasc85 <pasc.philipp@gmail.com>
2019-02-06 22:41:33 +0000

    Handled empty lines in dict and watchlist, and improved cwl.

--- cwl.py
+++ cwl.py
@@ -9,13 +9,18 @@ if os.path.isfile('watchlist.txt'):
     stocks = {}
     wl_file = open('watchlist.txt', 'r')
     for line in wl_file:
-        temp = line.strip().split(' ')
-        stocks[temp[0]] = float(temp[1])
+        if line.strip():
+            temp = line.strip().split(' ')
+            stocks[temp[0]] = float(temp[1])
     wl_file.close()
 
     # scrape stock prices and print alert message
     # if some of them are below the given prices
     alert = False
+    best_ratio = 100.00
+    best_name = ''
+    best_curr = 1.0
+    best_comp = 1.0
     for s in stocks.keys():
         source = "https://finance.yahoo.com/quote/"+s
         filehandle = urllib.request.urlopen(source)
@@ -27,11 +32,18 @@ if os.path.isfile('watchlist.txt'):
             if curr <= comp:
                 alert = True
                 print('ALERT!!!')
-                print('%s is currently %.3f, which is below %.3f.'
-                      % (s, curr, comp))
+                print('%s is currently %.3f < %.3f.' % (s, curr, comp))
+            else:
+                if curr/comp < best_ratio:
+                    best_name = s
+                    best_ratio = curr/comp
+                    best_curr = curr
+                    best_comp = comp
 
     if not alert:
         print('Nothing to alert to.')
+        print('(%s is closest with current price %.3f > %.3f.)'
+              % (best_name, best_curr, best_comp))
 
 else:
     print('File <watchlist.txt> not present.')
